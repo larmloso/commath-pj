f1d074b1fbc0fbb6689f2f34d11c1ddd2f65c761
ramsonram <thanakorn.ka.61@ubu.ac.th>
2020-10-13 18:47:44 +0700

    Updates

---
+++ precache-manifest.a744f38e117e754d06ba3bf3c1b8d837.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e564714bd8cc751ede94b0413814f88",
+    "url": "/commath-project/index.html"
+  },
+  {
+    "revision": "dd6eb4cdcd2832d8a728",
+    "url": "/commath-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "901583ee10f65197a134",
+    "url": "/commath-project/static/js/2.c24a221a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/commath-project/static/js/2.c24a221a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd6eb4cdcd2832d8a728",
+    "url": "/commath-project/static/js/main.d28c3482.chunk.js"
+  },
+  {
+    "revision": "4c4373a952a387ed5748",
+    "url": "/commath-project/static/js/runtime-main.b33480ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/commath-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
